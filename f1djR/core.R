ed7fe1aa671c3f9264342bf90805edce5a21d510
psychemedia <tony.hirst@gmail.com>
2012-09-23 10:29:44 +0100

    Refresh core library

--- f1djR/core.R
+++ f1djR/core.R
@@ -1,8 +1,22 @@
+#Dependencies
+#Take the aggressive stance of installing any packages that appear to be missing
+packages.list <- c("ggplot2","reshape","plyr")
+new.packages <- packages.list[!(packages.list %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+for (i in 1:length(packages.list)) {
+  library(packages.list[i], character.only = TRUE)
+}
 
-orderRaces=function (races) factor(races,levels=c('AUSTRALIA','MALAYSIA','CHINA','BAHRAIN','SPAIN','MONACO','CANADA','EUROPE','GREAT BRITAIN','GERMANY','HUNGARY','BELGIUM','ITALY'))
+
+#Ordering functions
+orderRaces=function (races) factor(races,levels=c('AUSTRALIA','MALAYSIA','CHINA','BAHRAIN','SPAIN','MONACO','CANADA','EUROPE','GREAT BRITAIN','GERMANY','HUNGARY','BELGIUM','ITALY','SINGAPORE'))
 orderTeams=function (teams) factor(teams,levels=c("Red Bull Racing-Renault","McLaren-Mercedes","Ferrari","Mercedes","Lotus-Renault","Force India-Mercedes","Sauber-Ferrari","STR-Ferrari","Williams-Renault","Caterham-Renault","HRT-Cosworth","Marussia-Cosworth"),ordered=T)
 tlid=data.frame(driverName=c("Sebastian Vettel","Mark Webber","Jenson Button", "Lewis Hamilton", "Fernando Alonso","Felipe Massa","Michael Schumacher","Nico Rosberg", "Kimi Räikkönen","Romain Grosjean","Jerome D'Ambrosio", "Paul di Resta", "Nico Hulkenberg","Kamui Kobayashi","Sergio Perez","Daniel Ricciardo","Jean-Eric Vergne","Pastor Maldonado","Bruno Senna","Heikki Kovalainen","Vitaly Petrov", "Pedro de la Rosa","Narain Karthikeyan","Timo Glock" ,"Charles Pic" ),TLID= c('VET','WEB','BUT','HAM','ALO','MAS','MSC','ROS','RAI','GRO','DAM','DIR','HUL','KOB','PER','RIC','VER','MAL','SEN','KOV','PET','DEL','KAR','GLO','PIC'))
 
+#ggplot chart helpers
+xRot=function(g,s=5,lab=NULL) g+theme(axis.text.x=element_text(angle=-90,size=s))+xlab(lab)
+
+#Data loaders
 floader=function(table){
   temporaryFile <- tempfile()
   fn=paste("https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=csv&name=f1comscraper&query=select+*+from+`",table,"`&apikey=",sep='')
@@ -10,6 +24,7 @@ floader=function(table){
   read.csv(temporaryFile)
 }
 
+#Data table annoations
 teamDriver=function(d) if (d>13) return(d %% 2) else return((1+d) %% 2)
 
 fetchAndAnnotate = function(t,race=NA){
