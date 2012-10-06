450e56ddb5c142974b6bda4715b282f0c22e9773
psychemedia <tony.hirst@gmail.com>
2012-10-06 01:29:09 +0100

    Updates to core routines

--- f1djR/core.R
+++ f1djR/core.R
@@ -9,17 +9,19 @@ for (i in 1:length(packages.list)) {
 
 
 #Ordering functions
-orderRaces=function (races) factor(races,levels=c('AUSTRALIA','MALAYSIA','CHINA','BAHRAIN','SPAIN','MONACO','CANADA','EUROPE','GREAT BRITAIN','GERMANY','HUNGARY','BELGIUM','ITALY','SINGAPORE'))
+orderRaces=function (races) factor(races,levels=c('AUSTRALIA','MALAYSIA','CHINA','BAHRAIN','SPAIN','MONACO','CANADA','EUROPE','GREAT BRITAIN','GERMANY','HUNGARY','BELGIUM','ITALY','SINGAPORE','JAPAN'))
 orderTeams=function (teams) factor(teams,levels=c("Red Bull Racing-Renault","McLaren-Mercedes","Ferrari","Mercedes","Lotus-Renault","Force India-Mercedes","Sauber-Ferrari","STR-Ferrari","Williams-Renault","Caterham-Renault","HRT-Cosworth","Marussia-Cosworth"),ordered=T)
-tlid=data.frame(driverName=c("Sebastian Vettel","Mark Webber","Jenson Button", "Lewis Hamilton", "Fernando Alonso","Felipe Massa","Michael Schumacher","Nico Rosberg", "Kimi Räikkönen","Romain Grosjean","Jerome D'Ambrosio", "Paul di Resta", "Nico Hulkenberg","Kamui Kobayashi","Sergio Perez","Daniel Ricciardo","Jean-Eric Vergne","Pastor Maldonado","Bruno Senna","Heikki Kovalainen","Vitaly Petrov", "Pedro de la Rosa","Narain Karthikeyan","Timo Glock" ,"Charles Pic" ),TLID= c('VET','WEB','BUT','HAM','ALO','MAS','MSC','ROS','RAI','GRO','DAM','DIR','HUL','KOB','PER','RIC','VER','MAL','SEN','KOV','PET','DEL','KAR','GLO','PIC'))
+tlid=data.frame(driverName=c("Sebastian Vettel","Mark Webber","Jenson Button", "Lewis Hamilton", "Fernando Alonso","Felipe Massa","Michael Schumacher","Nico Rosberg", "Kimi Räikkönen","Romain Grosjean","Jerome D'Ambrosio", "Paul di Resta", "Nico Hulkenberg","Kamui Kobayashi","Sergio Perez","Daniel Ricciardo","Jean-Eric Vergne","Pastor Maldonado","Bruno Senna","Heikki Kovalainen","Vitaly Petrov", "Pedro de la Rosa","Narain Karthikeyan","Timo Glock" ,"Charles Pic","Giedo van der Garde","Valtteri  Bottas" ),TLID= c('VET','WEB','BUT','HAM','ALO','MAS','MSC','ROS','RAI','GRO','DAM','DIR','HUL','KOB','PER','RIC','VER','MAL','SEN','KOV','PET','DEL','KAR','GLO','PIC','VDG','BOT'))
 
 #ggplot chart helpers
 xRot=function(g,s=5,lab=NULL) g+theme(axis.text.x=element_text(angle=-90,size=s))+xlab(lab)
 
 #Data loaders
-floader=function(table){
+floader=function(table,race=NULL){
   temporaryFile <- tempfile()
-  fn=paste("https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=csv&name=f1comscraper&query=select+*+from+`",table,"`&apikey=",sep='')
+  fn=paste("https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=csv&name=f1comscraper&query=select+*+from+`",table,"`",sep='')
+  if (!is.null(race)) fn=paste(fn,'%20where%20`race`%20like%20\'',toupper(race),'\'',sep='')
+  fn=paste(fn,"&apikey=",sep='')
   download.file(fn,destfile=temporaryFile, method="curl")
   read.csv(temporaryFile)
 }
@@ -44,3 +46,33 @@ fetchAndAnnotate = function(t,race=NA){
   d$intpos=as.integer(as.character(d$pos))
   return(d)
 }
+
+
+#----ORDERINGS
+f_driverOrderings=function(data){
+  data=merge(data,tlid,by='driverName')
+  data$driverName=reorder(data$driverName, data$driverNum)
+  data$TLID=reorder(data$TLID, data$driverNum)
+  return(data)
+}
+
+
+#---------PLOTS------
+
+f_sectorTimes=function(sessionData,stub=''){
+  g=ggplot(sessionData)+geom_point(aes(x=TLID,y=sectortime))+facet_wrap(~sector)
+  g=g+ggtitle(mktitle(stub))
+  g=xRot(g)
+  g=g+xlab(NULL)+ylab("Sectortime (s)")
+  print(g)
+}
+
+
+f_sectorTimesNorm=function(sessionData,stub=''){
+  g=ggplot(sessionData)+geom_point(aes(x=TLID,y=norm))+facet_wrap(~sector)
+  g=g+ggtitle(mktitle(stub))
+  g=xRot(g)
+  g=g+xlab(NULL)+ylab("Normalised sectortime")+scale_y_reverse()
+  print(g)
+}
+
