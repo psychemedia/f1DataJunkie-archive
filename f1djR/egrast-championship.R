c30624b5c8e46c7f8eeaac5d994aa1a66eb55a17
psychemedia <tony.hirst@gmail.com>
2012-11-17 00:30:56 +0000

    A couple of cleaner charts

--- f1djR/egrast-championship.R
+++ f1djR/egrast-championship.R
@@ -29,4 +29,15 @@ test2=subset(champ,( driverID=='vettel' | driverID=='alonso' | driverID=='raikko
 #Really rough sketch, in part inspired by http://junkcharts.typepad.com/junk_charts/2012/11/the-electoral-map-sans-the-map.html
 ggplot(test2)+geom_line(aes(x=round,y=points,group=driverID,col=driverID))+labs(title="F1 2012 - Race to the Championship")
 
+#How about in the style of a lapchart?
+ggplot(test2)+geom_line(aes(x=round,y=position,group=driverID,col=driverID))+labs(title="F1 2012 - Race to the Championship")
+
+
+#Clean things a little...
+test3=subset(test,( driverID=='vettel' | driverID=='alonso' ))
+test4=subset(test,( driverID=='raikkonen'|driverID=='webber' | driverID=='hamilton'|driverID=='button' ))
+
+ggplot(test4)+geom_line(aes(x=round,y=position,group=driverID),col='lightgrey')+geom_line(data=test3,aes(x=round,y=position,group=driverID,col=driverID))+labs(title="F1 2012 - Race to the Championship")
+> ggplot(test4)+geom_line(aes(x=round,y=points,group=driverID),col='lightgrey')+geom_line(data=test3,aes(x=round,y=points,group=driverID,col=driverID))+labs(title="F1 2012 - Race to the Championship")
+
 #I wonder if it would be worth annotating the chart with labels explaining any DNF reasons at parts where points stall?
